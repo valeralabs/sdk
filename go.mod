4074da4ddebaf4b88b8bce4b978daf7bbca36a9b
Linden <70739041+linden@users.noreply.github.com>
2022-06-26 16:58:43 -0700

    feat: use `github.com/linden/bite` for `[]byte` reading

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/valeralabs/racks
 
 go 1.18
+
+require github.com/linden/bite v0.0.0-20220626234907-1a027616b2bf
--- transaction/transaction.go
+++ transaction/transaction.go
@@ -6,17 +6,19 @@ import (
 	"encoding/binary"
 	"encoding/hex"
 	"errors"
+
+	"github.com/linden/bite"
 )
 
 type StacksTransaction struct {
-	Version           constant.TransactionVersion
-	ChainID           constant.ChainID
-	Payload           Payload
-	Authorization     Authorization
-	AnchorMode        constant.AnchorMode
-	PostConditionMode constant.PostConditionMode
-	//TODO: figure out what is in a PostCondtion
-	PostConditions []constant.PostCondition
+	Version       constant.TransactionVersion
+	ChainID       constant.ChainID
+	Payload       Payload
+	Authorization Authorization
+	AnchorMode    constant.AnchorMode
+	// TODO: add post-conditions
+	// PostConditionMode constant.PostConditionMode
+	// PostConditions    []constant.PostCondition
 }
 
 func HexToBytes(plain string) ([]byte, error) {
@@ -34,70 +36,47 @@ func (transaction *StacksTransaction) Unmarshal(data []byte) error {
 		return errors.New("transaction is too short to be valid")
 	}
 
-	// version byte is first byte
-	transaction.Version = constant.TransactionVersion(data[0])
+	reader := bite.New(data)
+
+	transaction.Version = constant.TransactionVersion(reader.ReadSingle())
 
 	if transaction.Version.Check() == false {
 		return errors.New("transaction version is invalid")
 	}
 
-	// version byte is first byte
-	data = data[1:]
-
-	// next 4 bytes are the chain ID
-	// TODO (Linden): change when Go hits 1.18 (https://stackoverflow.com/a/67199587)
-	transaction.ChainID = constant.ChainID(*(*[4]byte)(data[:4]))
+	// TODO (Linden): change when Go hits 1.19 (https://stackoverflow.com/a/67199587)
+	transaction.ChainID = constant.ChainID(*(*[4]byte)(reader.Read(4)))
 
 	if transaction.ChainID.Check() == false {
 		return errors.New("transaction chainID is invalid")
 	}
 
-	data = data[4:]
-
-	// next byte is the transaction authorization type
-	authorizationType := data[0]
-	data = data[1:]
+	authorizationType := reader.ReadSingle()
 
 	switch authorizationType {
 	case 0x04:
 		var condtion SingleSigSpendingCondition
 
-		condtion.HashMode = HashMode(data[0])
+		condtion.HashMode = HashMode(reader.ReadSingle())
 
 		if condtion.HashMode.Check() == false {
 			return errors.New("authorization must be signed with either P2PKH, P2SH, P2WPKH-P2SH or P2WSH-P2SH")
 		}
 
-		data = data[1:]
-
-		// 20-byte public key hash
-		condtion.Signer = *(*[20]byte)(data[:20])
-		data = data[20:]
-
-		// 8-byte nonce
-		condtion.Nonce = binary.BigEndian.Uint64(data[:8])
-		data = data[8:]
-
-		// 8-byte fee
-		condtion.Fee = binary.BigEndian.Uint64(data[:8])
-		data = data[8:]
+		condtion.Signer = *(*[20]byte)(reader.Read(20))
+		condtion.Nonce = binary.BigEndian.Uint64(reader.Read(8))
+		condtion.Fee = binary.BigEndian.Uint64(reader.Read(8))
 
 		switch condtion.HashMode {
-		// 00 or 02 - single signature
 		case HashModeP2PKH, HashModeP2WPKH_P2SH:
-			// next byte is the public key encoding
-			condtion.KeyEncoding = constant.PublicKeyEncoding(data[0])
-			data = data[1:]
+			condtion.KeyEncoding = constant.PublicKeyEncoding(reader.ReadSingle())
 
 			if condtion.KeyEncoding.Check() == false {
 				return errors.New("public key encoding must be compressed or uncompressed")
 			}
 
-			// next 65-byte is a recoverable ECDSA signature
-			condtion.Signature = *(*[65]byte)(data[:65])
-			data = data[65:]
+			condtion.Signature = *(*[65]byte)(reader.Read(65))
 
-		// 01 or 03 - multi signature
 		case HashModeP2SH, HashModeP2WSH_P2SH:
 			panic("TODO: implment HashModeP2SH and HashModeP2WSH_P2SH")
 		}
@@ -110,39 +89,29 @@ func (transaction *StacksTransaction) Unmarshal(data []byte) error {
 		panic("TODO: implment sponsored authorization")
 	}
 
-	// assuming we've set authorization and updated data to the next byte
-
-	// next byte is anchor mode
-	transaction.AnchorMode = constant.AnchorMode(data[0])
-	data = data[1:]
+	transaction.AnchorMode = constant.AnchorMode(reader.ReadSingle())
 
 	if transaction.AnchorMode.Check() == false {
 		return errors.New("anchor mode is invalid")
 	}
 
-	// next byte is post condition mode
-	transaction.PostConditionMode = constant.PostConditionMode(data[0])
-	data = data[1:]
-
-	if transaction.PostConditionMode.Check() == false {
-		return errors.New("post condition mode is invalid")
-	}
-
-	postConditionCount := binary.BigEndian.Uint32(data[:4])
-
-	// TODO: loop through the post conditions
-	for index := uint32(0); index < postConditionCount; index++ {
-		// first 1 byte is the post condition type ID
-		postConditionType := data[0]
-		data = data[1:]
-		switch postConditionType {
-		case 00: // post condition type is STX
-		case 01: // post condition type is fungible token
-		case 02: // post condition type is non-fungible token (NFT)
-		}
-	}
-
-	data = data[4:]
+	// // next byte is post condition mode
+	// transaction.PostConditionMode = constant.PostConditionMode(reader.ReadSingle())
+	// if transaction.PostConditionMode.Check() == false {
+	// 	return errors.New("post condition mode is invalid")
+	// }
+	// postConditionCount := binary.BigEndian.Uint32(reader.Read(4))
+	// // TODO: loop through the post conditions
+	// for index := uint32(0); index < postConditionCount; index++ {
+	// 	// first 1 byte is the post condition type ID
+	// 	postConditionType := data[0]
+	// 	data = data[1:]
+	// 	switch postConditionType {
+	// 	case 00: // post condition type is STX
+	// 	case 01: // post condition type is fungible token
+	// 	case 02: // post condition type is non-fungible token (NFT)
+	// 	}
+	// }
 
 	return nil
 
