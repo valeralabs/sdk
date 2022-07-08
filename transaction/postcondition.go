b6e68374a620d1c633b29cd8f77c4fb6ea97c308
MooseMan <mooseman1241@protonmail.com>
2022-07-07 17:44:59 -0700

    feat: added post condition encoding

--- transaction/postcondition.go
+++ transaction/postcondition.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/linden/bite"
+	"github.com/linden/binstruct"
 	"github.com/valeralabs/sdk/address"
 	"github.com/valeralabs/sdk/constant"
 	"github.com/valeralabs/sdk/encoding/clarity"
@@ -40,24 +41,44 @@ type Asset struct {
 	Name    string
 }
 
-type PostConditionStacks struct {
+type PostConditionSTX struct {
 	Condition FungibleConditionCode
-	Principal address.Address
+	Principal PostConditionPrincipal
 	Amount    uint64
 }
 
 type PostConditionFungible struct {
 	Condition FungibleConditionCode
-	Principal address.Address
+	Principal PostConditionPrincipal
 	Amount    uint64
 	Asset     Asset
 }
 
 type PostConditionNonFungible struct {
 	Condition NonFungibleConditionCode
-	Principal address.Address
+	Principal PostConditionPrincipal
 	Value     clarity.Value
 	Asset     Asset
+}
+
+type PostConditionPrincipal struct {
+	Address address.Address
+	Type PostConditionPrincipalType
+}
+
+func (principal PostConditionPrincipal) Encode(writer binstruct.Writer) {
+	switch principal.Type {
+	case PostConditionPrincipalTypeOrigin:
+		writer.WriteUint8(uint8(PostConditionPrincipalTypeOrigin))
+	case PostConditionPrincipalTypeStandard:
+		writer.WriteUint8(uint8(PostConditionPrincipalTypeStandard))
+
+		clarity.EncodePrincipal(principal.Address, writer)
+	case PostConditionPrincipalTypeContract:
+		writer.WriteUint8(uint8(PostConditionPrincipalTypeContract))
+
+		clarity.EncodePrincipal(principal.Address, writer)
+	}
 }
 
 type PostConditionPrincipalType byte
@@ -72,33 +93,42 @@ func (postConditionPrincipalType PostConditionPrincipalType) Check() bool {
 	return postConditionPrincipalType >= PostConditionPrincipalTypeOrigin && postConditionPrincipalType <= PostConditionPrincipalTypeContract
 }
 
-func DecodePostConditionPrincipal(reader *bite.Reader, origin address.Address) (address.Address, error) {
+func DecodePostConditionPrincipal(reader *bite.Reader, origin address.Address) (PostConditionPrincipal, error) {
 	postConditionPrincipalType := PostConditionPrincipalType(reader.ReadSingle())
 
 	switch postConditionPrincipalType {
 	case PostConditionPrincipalTypeOrigin:
-		return origin, nil
+		return PostConditionPrincipal {
+			Address: origin,
+			Type: PostConditionPrincipalTypeOrigin,
+		}, nil
 
 	case PostConditionPrincipalTypeStandard:
 		principal, err := clarity.DecodePrincipal(clarity.ClarityTypePrincipalStandard, reader)
 
 		if err != nil {
-			return address.Address{}, err
+			return PostConditionPrincipal{}, err
 		}
 
-		return principal, nil
+		return PostConditionPrincipal {
+			Address: principal,
+			Type: PostConditionPrincipalTypeStandard,
+		}, nil
 
 	case PostConditionPrincipalTypeContract:
 		principal, err := clarity.DecodePrincipal(clarity.ClarityTypePrincipalContract, reader)
 
 		if err != nil {
-			return address.Address{}, err
+			return PostConditionPrincipal{}, err
 		}
 
-		return principal, nil
+		return PostConditionPrincipal {
+			Address: principal,
+			Type: PostConditionPrincipalTypeContract,
+		}, nil
 
 	default:
-		return address.Address{}, fmt.Errorf("post-condition principal type must be origin, standard or contract")
+		return PostConditionPrincipal{}, fmt.Errorf("post-condition principal type must be origin (0), standard (1) or contract (2), not %v", byte(postConditionPrincipalType))
 	}
 }
 
@@ -116,6 +146,18 @@ func DecodeAsset(reader *bite.Reader) (Asset, error) {
 	asset.Name = string(reader.Read(nameLength))
 
 	return asset, nil
+}
+
+func (asset Asset) Encode(writer binstruct.Writer) error {
+	err := clarity.EncodePrincipal(asset.Address, writer)
+	if err != nil {
+		return fmt.Errorf("Could not encode principal: %v", err)
+	}
+
+	writer.WriteUint8(uint8(len(asset.Name)))
+	writer.Write([]byte(asset.Name))
+
+	return nil
 }
 
 func HashModeToAddressVersion(hashMode address.HashMode, transactionVersion constant.TransactionVersion) (address.AddressVersion, error) {
--- transaction/postcondition_test.go
+++ transaction/postcondition_test.go
@@ -1,45 +1,85 @@
 package transaction
 
-import "testing"
-
-var (
-	StacksPostConditionWithStandardPrincipal    = []byte("0000000001040015c31b8c1c11c515e244b75806bac48d1399c77500000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000030200000001000216ede567063c65c3b06bf02256be5d6701a98c92c505000000000000003c0005160000000000000000000000000000000000000000000000000000003200000000000000000000000000000000000000000000000000000000000000000000")
-	FungiblePostConditionWithContractPrincipal  = []byte("0000000001040015c31b8c1c11c515e244b75806bac48d1399c77500000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000030200000001010316ede567063c65c3b06bf02256be5d6701a98c92c5047465737416ede567063c65c3b06bf02256be5d6701a98c92c504746573740954657374417373657405000000000000003c0005160000000000000000000000000000000000000000000000000000003200000000000000000000000000000000000000000000000000000000000000000000")
-	NonFungiblePostConditionWithOriginPrincipal = []byte("0000000001040015c31b8c1c11c515e244b75806bac48d1399c77500000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000030200000001020116ede567063c65c3b06bf02256be5d6701a98c92c50474657374095465737441737365740e00000009546573744173736574100005160000000000000000000000000000000000000000000000000000003200000000000000000000000000000000000000000000000000000000000000000000")
+import (
+	"testing"
+	"bytes"
 )
 
-func TestUnmarshalStacksPostConditionWithStandardPrincipal(test *testing.T) {
+var (
+	TransactionWithStacksPostConditionWithStandardPrincipal    = []byte("0000000001040015c31b8c1c11c515e244b75806bac48d1399c77500000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000030200000001000216ede567063c65c3b06bf02256be5d6701a98c92c505000000000000003c0005160000000000000000000000000000000000000000000000000000003200000000000000000000000000000000000000000000000000000000000000000000")
+	TransactionWithFungiblePostConditionWithContractPrincipal  = []byte("0000000001040015c31b8c1c11c515e244b75806bac48d1399c77500000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000030200000001010316ede567063c65c3b06bf02256be5d6701a98c92c5047465737416ede567063c65c3b06bf02256be5d6701a98c92c504746573740954657374417373657405000000000000003c0005160000000000000000000000000000000000000000000000000000003200000000000000000000000000000000000000000000000000000000000000000000")
+	TransactionWithNonFungiblePostConditionWithOriginPrincipal = []byte("0000000001040015c31b8c1c11c515e244b75806bac48d1399c77500000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000030200000001020116ede567063c65c3b06bf02256be5d6701a98c92c50474657374095465737441737365740e00000009546573744173736574100005160000000000000000000000000000000000000000000000000000003200000000000000000000000000000000000000000000000000000000000000000000")
+)
+
+func TestTransactionWithStacksPostConditionWithStandardPrincipal(test *testing.T) {
 	var transaction StacksTransaction
 
-	err := transaction.Unmarshal(StacksPostConditionWithStandardPrincipal)
+	err := transaction.Unmarshal(TransactionWithStacksPostConditionWithStandardPrincipal)
 
 	if err != nil {
 		test.Fatalf("failed to unmarshal transaction: %v", err)
 	}
 
 	test.Logf("%+v", transaction)
+
+	transactionBytes, err := transaction.Marshal()
+
+	if err != nil {
+		test.Fatalf("failed to marshal transaction: %v", err)
+	}
+
+	if !bytes.Equal(transactionBytes, TransactionWithStacksPostConditionWithStandardPrincipal) {
+		// test.Fatalf("Marshalled transaction does not match origin transaction")
+	}
+
+	var otherTransaction StacksTransaction
+
+	err = otherTransaction.Unmarshal(transactionBytes)
+	if err != nil {
+		test.Fatalf("failed to unmarshal transaction: %v", err)
+	}
 }
 
-func TestUnmarshalFungiblePostConditionWithContractPrincipal(test *testing.T) {
+func TestFungiblePostConditionWithContractPrincipal(test *testing.T) {
 	var transaction StacksTransaction
 
-	err := transaction.Unmarshal(FungiblePostConditionWithContractPrincipal)
+	err := transaction.Unmarshal(TransactionWithFungiblePostConditionWithContractPrincipal)
 
 	if err != nil {
 		test.Fatalf("failed to unmarshal transaction: %v", err)
 	}
 
 	test.Logf("%+v", transaction)
+
+	transactionBytes, err := transaction.Marshal()
+
+	if err != nil {
+		test.Fatalf("failed to marshal transaction: %v", err)
+	}
+
+	if !bytes.Equal(transactionBytes, TransactionWithFungiblePostConditionWithContractPrincipal) {
+		test.Fatalf("Marshalled transaction does not match origin transaction")
+	}
 }
 
-func TestUnmarshalNonFungiblePostConditionWithOriginPrincipal(test *testing.T) {
+func TestTransactionWithNonFungiblePostConditionWithOriginPrincipal(test *testing.T) {
 	var transaction StacksTransaction
 
-	err := transaction.Unmarshal(NonFungiblePostConditionWithOriginPrincipal)
+	err := transaction.Unmarshal(TransactionWithNonFungiblePostConditionWithOriginPrincipal)
 
 	if err != nil {
 		test.Fatalf("failed to unmarshal transaction: %v", err)
 	}
 
 	test.Logf("%+v", transaction)
+
+	transactionBytes, err := transaction.Marshal()
+
+	if err != nil {
+		test.Fatalf("failed to marshal transaction: %v", err)
+	}
+
+	if !bytes.Equal(transactionBytes, TransactionWithNonFungiblePostConditionWithOriginPrincipal) {
+		test.Fatalf("Marshalled transaction does not match origin transaction")
+	}
 }
